8bc45d35caf5af0eb0674e468f4d3a866a7131a2
SSuttie <seanpaulsuttie@gmail.com>
2016-02-02 17:09:53 +0000

    Update SimplePrimeSieve.m

--- SimplePrimeSieve.m
+++ SimplePrimeSieve.m
@@ -5,10 +5,10 @@ close all
 clear all
 limit = sqrt(600851475143);
 in = 1;
-for n = 1:2:limit
+for n = 1:1:limit
     b(n) = 0;
     
-    for m = 1:2:n - 1
+    for m = 2:1:n - 1
         if(mod(n,m) == 0)
         b(n) = 1;
         end
